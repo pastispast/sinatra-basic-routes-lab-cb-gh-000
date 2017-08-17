3a3d44bff74f171868d983b5c6e8a302078c7725
Nosredna  Dash <ajmanosalvav@unal.edu.co>
2017-08-17 20:17:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nosredna"
+  end
+
+  get '/hometown' do
+    "My hometown is Hiperborea"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 661"
+  end
 end
